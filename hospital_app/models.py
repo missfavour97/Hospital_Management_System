e4a7103fa920eb3521ed5f6e8e92a93eca738f6d
missfavour97 <kemwilson574@icloud.com>
2026-04-10 22:55:13 +0300

    Add Patient model with fields and initial migration

--- hospital_app/models.py
+++ hospital_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=10)
+    date_of_birth = models.DateField()
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    address = models.TextField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
