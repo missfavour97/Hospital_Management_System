2b1d1be56ea8c9e4fb9fcdc4c676c31de991eeb7
missfavour97 <kemwilson574@icloud.com>
2026-04-10 23:14:49 +0300

    Register Doctor model in admin and create views and URLs for patient management

--- hospital_app/admin.py
+++ hospital_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Patient
+from .models import Patient, Doctor
 
 admin.site.register(Patient)
+admin.site.register(Doctor)
--- hospital_app/views.py
+++ hospital_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Patient
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def patient_list(request):
+    patients = Patient.objects.all()
+    return render(request, 'patients.html', {'patients': patients})
