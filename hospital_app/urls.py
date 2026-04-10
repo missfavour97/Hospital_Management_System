7173729901af031f8e97107754a130b4d5fb5750
missfavour97 <kemwilson574@icloud.com>
2026-04-11 00:13:28 +0300

    Enhance home page layout and styling; add patient management URL

--- hospital_app/urls.py
+++ hospital_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('patients/', views.patient_list, name='patient_list'),
+    path('add-patient/', views.add_patient, name='add_patient'),
 ]
