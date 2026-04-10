348768db25c8f7798bdc44e6e936b1b0461513d8
missfavour97 <kemwilson574@icloud.com>
2026-04-11 02:00:52 +0300

    Reorganize home page layout; group management links for departments, doctors, patients, appointments, and prescriptions

--- hospital_app/admin.py
+++ hospital_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin 
 from .models import Patient, Doctor, Department, Appointment, Prescription
 
-admin.site.register(Patient)
-admin.site.register(Doctor)
 admin.site.register(Department)
+admin.site.register(Doctor)
+admin.site.register(Patient)
 admin.site.register(Appointment)
 admin.site.register(Prescription)
