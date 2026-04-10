8149307a159ec14530024b99c3c13618b6788199
missfavour97 <kemwilson574@icloud.com>
2026-04-10 23:58:05 +0300

    Add patient management functionality with form and template

--- hospital_app/views.py
+++ hospital_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Patient
+from .forms import PatientForm
 
 def home(request):
     return render(request, 'home.html')
@@ -7,3 +8,13 @@ def home(request):
 def patient_list(request):
     patients = Patient.objects.all()
     return render(request, 'patients.html', {'patients': patients})
+
+def add_patient(request):
+    if request.method == 'POST':
+        form = PatientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('patient_list')
+    else:
+        form = PatientForm()
+    return render(request, 'add_patient.html', {'form': form})
